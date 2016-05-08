dc544d829b311c0f96472127741d3671fe9df9f5
Madeleine Lindow <madeleine.lindow@gmail.com>
2016-05-08 23:43:35 +0200

    Making the bot a nice little home #5

---
+++ bot_home.py
@@ -0,0 +1,10 @@
+## Hi
+
+## This is the Github Commit Bot file. It accesses PyGithub and nltk.
+
+## Made by Madeleine Lindow, CDATE12, Project i INDAN DD1349 
+
+import nltk
+from github import Github
+
+g = Github("user", "password")
